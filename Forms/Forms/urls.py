4b23b11c22ae5782b4d8330d5512f4a59f3ed27f
Kovvuri Venkata Rama Durgaprasad Reddy <69207735+venkataramadurgaprasad@users.noreply.github.com>
2020-09-13 20:05:28 +0530

    Add files via upload

---
+++ Forms/Forms/urls.py
@@ -0,0 +1,29 @@
+"""Forms URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from MyApp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home,name='home'),
+    path('register/',views.register,name='register'),
+    path('status/',views.status,name='status'),
+    path('show/<int:id>',views.show,name='show'),
+    path('edit/<int:id>',views.edit,name='edit'),
+    path('delete/<int:id>',views.delete,name='delete'),
+    path('confirm/<int:id>',views.confirm,name='confirm'),
+]
